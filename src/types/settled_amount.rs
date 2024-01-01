f029bccd2cc8b64ad43b1d089634b691c7c27abb
leruaa <aurelien.catinon@gmail.com>
2024-01-01 20:30:08 +0000

    Set settled amount in

--- src/types/settled_amount.rs
+++ src/types/settled_amount.rs
@@ -4,6 +4,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SettledAmount {
+    pub token_in: Option<Address>,
+    pub amount_in: Option<U256>,
     pub token_out: Address,
     pub amount_out: U256,
 }
