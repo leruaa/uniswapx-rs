f2f62c45162340c4a34c10cd9beb500149d73e28
leruaa <aurelien.catinon@gmail.com>
2024-01-03 17:41:44 +0000

    Simplify test

--- tests/get_fill_events.rs
+++ tests/get_fill_events.rs
@@ -1,8 +1,7 @@
 use std::{env, sync::Arc};
 
 use alloy_providers::provider::Provider;
-use alloy_transport::BoxTransport;
-use alloy_transport_http::Http;
+use alloy_rpc_client::RpcClient;
 use dotenv::dotenv;
 use uniswapx::ReactorClient;
 
@@ -11,13 +10,15 @@ async fn test_get_fill_events() {
     dotenv().ok();
     let eth_rpc = env::var("ETH_RPC").unwrap();
 
-    let transport = Http::<reqwest::Client>::new(eth_rpc.parse().unwrap());
-    let transport = BoxTransport::new(transport);
-    let provider = Arc::new(Provider::new(transport));
+    let provider = Provider::new_with_client(
+        RpcClient::builder()
+            .reqwest_http(eth_rpc.parse().unwrap())
+            .boxed(),
+    );
     let reactor_client = ReactorClient::new(1);
 
     let events = reactor_client
-        .get_fill_events(provider, 18270815, Some(18270840))
+        .get_fill_events(Arc::new(provider), 18270815, Some(18270840))
         .await;
 
     println!("{events:#?}");
