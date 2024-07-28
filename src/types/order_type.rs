bc0fe1ca004a9f0dac0ddb92d31da460a939799d
leruaa <aurelien.catinon@gmail.com>
2024-07-28 11:18:43 +0000

    feat: add new order types

--- src/types/order_type.rs
+++ src/types/order_type.rs
@@ -2,7 +2,13 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub enum OrderType {
-    #[default]
     Dutch,
-    DutchLimit,
+    #[serde(rename = "Dutch_V2")]
+    DutchV2,
+    Limit,
+    Relay,
+    #[default]
+    #[serde(rename = "Dutch_V1_V2")]
+    DutchV1V2,
+    Priority,
 }
